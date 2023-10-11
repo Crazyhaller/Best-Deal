f9311fddb029ec58c078edbc345c17c0b20ccb4c
crazyhaller <suvigyamadrid@gmail.com>
2023-10-11 20:10:55 +0530

    add amazon product valid link check on search bar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,8 +1,45 @@
 'use client'
+import { FormEvent, useState } from 'react'
+
+const isValidAmazonProductURL = (link: string) => {
+  try {
+    const parsedURL = new URL(link)
+    const hostname = parsedURL.hostname
+
+    if (
+      hostname.includes('amazon.com') ||
+      hostname.includes('amazon.') ||
+      hostname.endsWith('amazon')
+    ) {
+      return true
+    }
+  } catch (error) {
+    return false
+  }
+}
 
 const SearchBar = () => {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const [searchPrompt, setSearchPrompt] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+
+    const isValidLink = isValidAmazonProductURL(searchPrompt)
+
+    if (!isValidLink) {
+      alert('Please enter a valid Amazon product link')
+      return
+    }
+
+    try {
+      setIsLoading(true)
+      // Write scraping logic here
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsLoading(false)
+    }
   }
 
   return (
@@ -11,9 +48,15 @@ const SearchBar = () => {
         type="text"
         placeholder="Enter product link"
         className="searchbar-input"
+        value={searchPrompt}
+        onChange={(e) => setSearchPrompt(e.target.value)}
       />
-      <button type="submit" className="searchbar-btn">
-        Search
+      <button
+        type="submit"
+        className="searchbar-btn"
+        disabled={searchPrompt === '' || isLoading}
+      >
+        {isLoading ? 'Searching...' : 'Search'}
       </button>
     </form>
   )
