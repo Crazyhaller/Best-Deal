fb6a9637d1f2530e00f5bb64c5c167c8ebced497
crazyhaller <suvigyamadrid@gmail.com>
2023-10-11 19:57:42 +0530

    add search bar and carousel component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import HeroCarousel from '@/components/HeroCarousel'
+import SearchBar from '@/components/SearchBar'
 import Image from 'next/image'
 import React from 'react'
 
 const Home = () => {
   return (
     <>
-      <section className="px-6 md:px-20 py-24 border-2 border-red-500">
+      <section className="px-6 md:px-20 py-24">
         <div className="flex max-xl:flex-col gap-16">
           <div className="flex flex-col justify-center">
             <p className="small-text">
@@ -24,9 +26,9 @@ const Home = () => {
               Powerful, self-serve product and growth analytics to help you
               convert, engage, and retain more.
             </p>
-            SearchBar
+            <SearchBar />
           </div>
-          HeroCarousel
+          <HeroCarousel />
         </div>
       </section>
 
@@ -34,7 +36,7 @@ const Home = () => {
         <h2 className="section-text">Trending</h2>
         <div className="flex flex-wrap gap-x-8 gap-y-16">
           {['Apple Iphone', 'Book', 'AirPods'].map((product) => (
-            <div>{product}</div>
+            <div key={product}>{product}</div>
           ))}
         </div>
       </section>
