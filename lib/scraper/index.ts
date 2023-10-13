2472412512a8c38d88cd5757a3524b329b778ac9
crazyhaller <suvigyamadrid@gmail.com>
2023-10-13 17:51:48 +0530

    get scraped data and make a data object

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import * as cheerio from 'cheerio'
-import { extractPrice } from '../utils'
+import { extractCurrency, extractPrice } from '../utils'
 
 export async function scrapeAmazonProduct(url: string) {
   if (!url) return
@@ -28,14 +28,51 @@ export async function scrapeAmazonProduct(url: string) {
 
     // Extract the product title
     const title = $('#productTitle').text().trim()
+
     const currentPrice = extractPrice(
       $('.priceToPay span.a-price-whole'),
       $('a.size.base.a-color-price'),
-      $('.a-button-selected .a-color-base'),
-      $('a.price.a-text-price')
+      $('.a-button-selected .a-color-base')
     )
 
-    console.log({ title, currentPrice })
+    const originalPrice = extractPrice(
+      $('#priceblock_ourprice'),
+      $('.a-price.a-text-price span.a-offscreen'),
+      $('#listPrice'),
+      $('#priceblock_dealprice'),
+      $('.a-size-base.a-color-price')
+    )
+
+    const outOfStock =
+      $('#availability span').text().trim().toLowerCase() ===
+      'Currently unavailable.'
+
+    const images =
+      $('#imgBlkFront').attr('data-a-dynamic-image') ||
+      $('#landingImage').attr('data-a-dynamic-image') ||
+      '{}'
+
+    const imageUrls = Object.keys(JSON.parse(images))
+
+    const currency = extractCurrency($('.a-price-symbol'))
+
+    const discountRate = $('.savingsPercentage').text().replace(/[-%]/g, '')
+
+    // Construct data object with scraped data
+    const data = {
+      url,
+      currency: currency || '$',
+      image: imageUrls[0],
+      title,
+      currentPrice: Number(currentPrice) || Number(originalPrice),
+      originalPrice: Number(originalPrice) || Number(currentPrice),
+      priceHistory: [],
+      discountRate: Number(discountRate),
+      category: 'category',
+      reviewsCount: 100,
+      stars: 4.5,
+      isOutOfStock: outOfStock,
+    }
   } catch (error: any) {
     throw new Error(`Failed to scrape Amazon product: ${error.message}`)
   }
