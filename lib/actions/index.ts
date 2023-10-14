4c658b5dbc9e0e2fb27ed96801ee6972885a976a
crazyhaller <suvigyamadrid@gmail.com>
2023-10-14 20:44:36 +0530

    setup mongo database and store scraped products

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,15 +1,61 @@
 'use server'
 
+import { revalidatePath } from 'next/cache'
 import { scrapeAmazonProduct } from '../scraper'
+import { connectDB } from '../mongoose'
+import Product from '../models/product.model'
+import { getHighestPrice, getLowestPrice, getAveragePrice } from '../utils'
 
 export async function scrapeAndStoreProduct(productUrL: string) {
   if (!productUrL) return
 
   try {
+    connectDB()
+
     const scrapedProduct = await scrapeAmazonProduct(productUrL)
 
     if (!scrapedProduct) return
+
+    let product = scrapedProduct
+
+    const existingProduct = await Product.findOne({ url: scrapedProduct.url })
+
+    if (existingProduct) {
+      const updatedPriceHistory: any = [
+        ...existingProduct.priceHistory,
+        { price: scrapedProduct.currentPrice },
+      ]
+      product = {
+        ...scrapedProduct,
+        priceHistory: updatedPriceHistory,
+        lowestPrice: getLowestPrice(updatedPriceHistory),
+        highestPrice: getHighestPrice(updatedPriceHistory),
+        averagePrice: getAveragePrice(updatedPriceHistory),
+      }
+    }
+
+    const newProduct = await Product.findOneAndUpdate(
+      { url: scrapedProduct.url },
+      product,
+      { new: true, upsert: true }
+    )
+
+    revalidatePath(`/product/${newProduct._id}`)
   } catch (error: any) {
     throw new Error(`Failed to create/update product: ${error.message}`)
+  }
+}
+
+export async function getProductById(productId: string) {
+  try {
+    connectDB()
+
+    const product = await Product.findOne({ _id: productId })
+
+    if (!product) return null
+
+    return product
+  } catch (error: any) {
+    throw new Error(`Failed to get product: ${error.message}`)
   }
 }
