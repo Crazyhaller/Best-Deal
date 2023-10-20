455f04b5000de27e4c32b020a37bd10110dc02fd
crazyhaller <suvigyamadrid@gmail.com>
2023-10-20 18:47:56 +0530

    finish product details page

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -73,3 +73,21 @@ export async function getAllProducts() {
     throw new Error(`Failed to get products: ${error.message}`)
   }
 }
+
+export async function getSimilarProducts(productId: string) {
+  try {
+    connectDB()
+
+    const currentProduct = await Product.findById(productId)
+
+    if (!currentProduct) return null
+
+    const similarProducts = await Product.find({
+      _id: { $ne: productId },
+    }).limit(3)
+
+    return similarProducts
+  } catch (error) {
+    console.log(error)
+  }
+}
